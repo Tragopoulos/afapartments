8f692bc034b159d4ba7eaf6545a1fee32e06dc2f
Fotios Tragopoulos <tragopoulos@icloud.com>
2019-04-16 20:10:04 +0100

    Photos Page ready

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { PhotosComponent } from './pages/photos/photos.component';
+import { ContactComponent } from './pages/contact/contact.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'photos', component: PhotosComponent},
+  {path: 'contact', component: ContactComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './template/navbar/navbar.component';
+import { FooterComponent } from './template/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PhotosComponent } from './pages/photos/photos.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    PhotosComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
